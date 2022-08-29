64290d4454c6eecd24faef2bccf5329e424c5fc9
larrysfinolb <larrysfinolb@gmail.com>
2022-08-29 08:26:42 -0400

    se remplazo 'requite' por 'require' en la linea 3 del index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,11 @@
 const profileRouter = require('./profileRouter');
 const bookcasesRouter = require('./bookcasesRouter');
-const booksRouter = requite('./booksRouter')
+const booksRouter = require('./booksRouter');
 
 const router = (app) => {
 	app.use('/api/profile', profileRouter);
 	app.use('/api/bookcases', bookcasesRouter);
-	app.use('/api/books', booksRouter );
+	app.use('/api/books', booksRouter);
 };
 
 module.exports = router;
